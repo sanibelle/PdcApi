7c310da63884e25f91851a9c0e4279de021ec101
antoine chagnon michaud <antoine.chagnonmichaud@cegeprdl.ca>
2026-04-08 22:23:17 -0400

    front-end functionnal

--- WebAPp/app/composables/services/ChangeRecordClient.ts
+++ WebAPp/app/composables/services/ChangeRecordClient.ts
@@ -3,7 +3,7 @@ import { useApi } from './ApiClient';
 export const useChangeRecordClient = () => {
   const api = useApi();
   const publishChangeRecord = async (changeRecordId: string): Promise<void> => {
-    await api.Post(`/changeRecords/${changeRecordId}/publish`, {});
+    await api.Post(`/changeRecords/publish/${changeRecordId}`, {});
   };
 
   return { publishChangeRecord };
