b1df0e04cb8c3230e86e77027b5e40d874a313ff
antoine chagnon michaud <antoine.chagnonmichaud@cegeprdl.ca>
2026-04-10 22:33:54 -0400

    .

--- Pdc.Domain/Interfaces/UseCases/ChangeRecord/IAddComplementaryInformationUseCase.cs
+++ Pdc.Domain/Interfaces/UseCases/ChangeRecord/IAddComplementaryInformationUseCase.cs
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:Pdc.Domain/Interfaces/UseCases/Versioning/IAddComplementaryInformationUseCase.cs
 ﻿namespace Pdc.Domain.Interfaces.UseCases.Versioning;
-========
-﻿namespace Pdc.Domain.Interfaces.UseCases.ChangeRecord;
->>>>>>>> 2f52bb1bcb77bc73a5f532a55cf759a5d4c44124:Pdc.Domain/Interfaces/UseCases/ChangeRecord/IAddComplementaryInformationUseCase.cs
 
 using Pdc.Domain.DTOS.Common;
 using Pdc.Domain.Models.Security;
--- Pdc.Domain/Interfaces/UseCases/Versioning/IDeleteComplementaryInformationUseCase.cs
+++ Pdc.Domain/Interfaces/UseCases/Versioning/IDeleteComplementaryInformationUseCase.cs
@@ -1,9 +1,5 @@
 ﻿
-<<<<<<<< HEAD:Pdc.Domain/Interfaces/UseCases/Versioning/IDeleteComplementaryInformationUseCase.cs
 namespace Pdc.Domain.Interfaces.UseCases.Versioning;
-========
-namespace Pdc.Domain.Interfaces.UseCases.ChangeRecord;
->>>>>>>> 2f52bb1bcb77bc73a5f532a55cf759a5d4c44124:Pdc.Domain/Interfaces/UseCases/ChangeRecord/IDeleteComplementaryInformationUseCase.cs
 
 public interface IDeleteComplementaryInformationUseCase
 {
